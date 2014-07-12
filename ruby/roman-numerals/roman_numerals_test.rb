ef66758bf2a57e1d5e679c0dfc77a1cc50f609a5
Julian Nicholls <juliannicholls29@gmail.com>
2014-07-12 21:15:56 +0100

    Enabled last two tests

--- ruby/roman-numerals/roman_numerals_test.rb
+++ ruby/roman-numerals/roman_numerals_test.rb
@@ -70,12 +70,10 @@ class RomanTest < MiniTest::Test
   end
 
   def test_1024
-    skip
     assert_equal 'MXXIV', 1024.to_roman
   end
 
   def test_3000
-    skip
     assert_equal 'MMM', 3000.to_roman
   end
 
